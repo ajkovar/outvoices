c89a8e12f955fbbdb09f6f4c9758654d6ee90536
Alex Kovar <ajkovar@gmail.com>
2020-09-10 07:29:30 -0500

    render personal info by lines

--- Main.hs
+++ Main.hs
@@ -17,26 +17,34 @@ import Data.Aeson as Aeson
 -- toPDFString :: T.Text -> PDFString
 -- toPDFString = PDFString . encodeUtf16BE
 
-fontDebug :: PDFFont -> T.Text -> Draw ()
-fontDebug theFont@(PDFFont f s) t = do
+renderLine :: T.Text -> PDFText ()
+renderLine t = do
+  displayText t
+  startNewLine
+
+renderLines :: PDFFont -> [T.Text] -> Draw [()]
+renderLines theFont@(PDFFont f s) lines = do
   drawText $ do
     setFont theFont
     textStart 10 702.0
     leading $ getHeight f s
     renderMode FillText
-    displayText t
-        --  startNewLine
-        --  displayText "Another little test"
+    mapM renderLine lines 
+    -- let renderLine t = do
+    --     displayText t
+    --     startNewLine
+    -- in mapM renderLine lines
     --  strokeColor $ Rgb 1 0 0
     --  stroke $ Line 10 200 612 200
     --  fill $ Circle 10 200 10
     --  stroke $ Rectangle (10 :+ (200.0 - (getDescent f s))) ((10.0 + textWidth theFont t) :+ (200.0 - getDescent f s + getHeight f s))
 
-textTest :: T.Text -> AnyFont -> Draw ()
-textTest name timesRoman  = do
+renderMyInfo :: Person -> AnyFont -> Draw [()]
+renderMyInfo person timesRoman  = do
+  -- (Person.name $ person)
   strokeColor red
   fillColor blue
-  fontDebug (PDFFont timesRoman 48) (name)
+  renderLines (PDFFont timesRoman 12) [(Person.name $ person), (Person.address $ person)]
 
 main :: IO()
 main = do
@@ -49,7 +57,7 @@ main = do
     page1 <- addPage Nothing
     newSection  "Text encoding" Nothing Nothing $ do
       drawWithPage page1 $ do
-          textTest (Person.name $ person) timesRoman
+          renderMyInfo person timesRoman
           drawText $ do startNewLine
           let style = Font (PDFFont timesRoman 8) red red
               rect = Rectangle (310 :+ 780) (510 :+ 790) 
