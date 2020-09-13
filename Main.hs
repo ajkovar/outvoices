9ef8fbb287a91244e834bd6d2c5ebf29b2ba5c48
Alex Kovar <ajkovar@gmail.com>
2020-09-13 14:05:21 -0500

    read hours from timesheet and calculate amount due

--- Main.hs
+++ Main.hs
@@ -12,6 +12,9 @@ import System.IO
 import Data.ByteString.Lazy as LBS
 import Data.Aeson as Aeson
 import qualified System.Console.CmdArgs as CArgs
+import Data.Csv as Csv
+import qualified Data.Vector as V
+import qualified Timesheet
 
 kingFisherDaisy :: PDF.Color
 kingFisherDaisy = PDF.Rgb 0.32 0.11 0.52
@@ -19,15 +22,24 @@ kingFisherDaisy = PDF.Rgb 0.32 0.11 0.52
 data OutVoice = OutVoice {client_name :: String, 
                           issue_date :: String, 
                           due_date :: String,
-                          invoice_number :: String
+                          invoice_number :: String,
+                          timesheet_file :: String,
+                          rate :: Double
                          } deriving (Show, CArgs.Data, CArgs.Typeable)
 
 outvoice = OutVoice{
                       client_name = CArgs.def CArgs.&= CArgs.help "Client to generate for (should match their data directory name)",
                       issue_date = CArgs.def CArgs.&= CArgs.help "Issue date",
                       due_date = CArgs.def CArgs.&= CArgs.help "Due date",
-                      invoice_number = CArgs.def CArgs.&= CArgs.help "Invoice Number"
+                      invoice_number = CArgs.def CArgs.&= CArgs.help "Invoice Number",
+                      timesheet_file = CArgs.def CArgs.&= CArgs.help "Location of CSV timesheet file",
+                      rate = CArgs.def CArgs.&= CArgs.help "Hourly billing Rate"
                    } CArgs.&= CArgs.summary "Generate an invoice based on a csv input file"
+
+setColor :: PDF.Color -> PDF.Draw ()
+setColor color = do
+  PDF.strokeColor color
+  PDF.fillColor color
 
 displayLine :: T.Text -> PDF.PDFText ()
 displayLine t = do
@@ -57,8 +69,7 @@ drawLines theFont@(PDF.PDFFont f s) lines x y = do
 
 renderMyInfo :: Person -> PDF.AnyFont -> PDF.Draw [()]
 renderMyInfo person timesRoman  = do
-  PDF.strokeColor PDF.black
-  PDF.fillColor PDF.black
+  setColor PDF.black
   let font = PDF.PDFFont timesRoman 10
   let nameAndNumber = (fmap (\f -> f person) [Person.name, Person.telephone])
   drawLines font nameAndNumber 400 730
@@ -67,28 +78,33 @@ renderMyInfo person timesRoman  = do
 renderClientInfo :: Person -> PDF.AnyFont -> PDF.Draw [()]
 renderClientInfo client timesRoman  = do
   let font = PDF.PDFFont timesRoman 10
-  PDF.strokeColor kingFisherDaisy
-  PDF.fillColor kingFisherDaisy
+  setColor kingFisherDaisy
   drawLine font "Billed To" 30 600
-  PDF.strokeColor PDF.black
-  PDF.fillColor PDF.black
+  setColor PDF.black
   let fields = [Person.name client] ++ (Person.addressFields client)
   drawLines font fields 30 587
 
 renderTitledLine :: PDF.AnyFont -> T.Text -> T.Text -> Double -> Double -> PDF.Draw ()
 renderTitledLine fontType title line x y = do
   let font = PDF.PDFFont fontType 10
-  PDF.strokeColor kingFisherDaisy
-  PDF.fillColor kingFisherDaisy
+  setColor kingFisherDaisy
   drawLine font title x y
-  PDF.strokeColor PDF.black
-  PDF.fillColor PDF.black
+  setColor PDF.black
   drawLine font line x (y - 13)
+
+renderAmountDue :: PDF.AnyFont -> T.Text -> T.Text -> PDF.Draw ()
+renderAmountDue fontType title line = do
+--   PDF.setJustification PDF.RightJustification
+  setColor kingFisherDaisy
+  drawLine (PDF.PDFFont fontType 10) title 480 600
+  setColor PDF.black
+  drawLine (PDF.PDFFont fontType 19) line 480 (600 - 17)
 
 main :: IO()
 main = do
   userArgs <- CArgs.cmdArgs outvoice
   contents <- LBS.readFile "data/me.json"
+  csvData <- LBS.readFile (timesheet_file userArgs)
   clientContents <- LBS.readFile $ "data/" ++ (client_name userArgs) ++ "/info.json"
   let (Just person) = Aeson.decode contents :: Maybe Person
   let (Just client) = Aeson.decode clientContents :: Maybe Person
@@ -104,6 +120,11 @@ main = do
           renderTitledLine timesRoman "Date of Issue" (T.pack $ issue_date userArgs) 180 600
           renderTitledLine timesRoman "Due Date" (T.pack $ issue_date userArgs) 180 560
           renderTitledLine timesRoman "Invoice Number" (T.pack $ issue_date userArgs) 280 600
+          case decodeByName csvData of
+            Left err -> return () -- System.IO.putStrLn err
+            Right (_, v) -> do
+              let total = V.foldr (\sheet s -> s + (Timesheet.hours sheet)) 0 v
+              renderAmountDue timesRoman "Amount Due" (T.pack $ show (total * (rate userArgs)))
           PDF.drawText $ do PDF.startNewLine
         --   let style = Font (PDF.PDFFont timesRoman 8) PDF.red PDF.red
         --       rect = PDF.Rectangle (310 PDF.:+ 780) (510 PDF.:+ 790) 
---
+++ Timesheet.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Timesheet where
+
+import Data.Csv
+
+data Timesheet = Timesheet
+    { date   :: !String
+    , client :: !String
+    , project :: !String
+    , task :: !String
+    , notes :: !String
+    , hours :: !Double
+    }
+
+instance FromNamedRecord Timesheet where
+    parseNamedRecord r = Timesheet <$> r .: "Date" <*> r .: "Client" <*> r .: "Project" <*> r .: "Task" <*> r .: "Notes" <*> r .: "Hours"
