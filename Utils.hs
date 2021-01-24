338667bcf7d036e31d94f9a7f6c77bcfb070bd09
Alex Kovar <ajkovar@gmail.com>
2021-01-24 15:01:14 -0500

    created pagination fn

--- Utils.hs
+++ Utils.hs
@@ -1,16 +1,33 @@
 
 module Utils where
 
-import Data.List.Split (splitEvery)
+import Data.List.Split (chunksOf)
 import Data.List (intercalate)
+import Prelude hiding (drop)
+import Data.Vector.Generic (Vector, drop, cons)
+import qualified Data.Vector.Generic as V
+import qualified Data.Vector.Split as VSplit
 
 formatMoney :: Double -> String
 formatMoney x = h++t
   where
     sp = break (== '.') $ show x
-    h = reverse (intercalate "," $ splitEvery 3 $ reverse $ fst sp) 
+    h = reverse (intercalate "," $ chunksOf 3 $ reverse $ fst sp) 
     decimal = snd sp
     t = case length decimal of 
       3 -> decimal
       2 -> decimal ++ "0"
       _ -> take 3 decimal
+
+paginate :: Vector v a => v a -> [v a]
+paginate v
+  | V.length v > firstPage = V.take firstPage v : VSplit.chunksOf rest (drop rest v)
+  | otherwise = [v] 
+  where firstPage = 15
+        rest = 30
+
+  
+
+
+
+
